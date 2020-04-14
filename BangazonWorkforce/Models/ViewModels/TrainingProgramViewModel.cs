f86b7c51cbf8286f2cef86c9a22bbd455e513c2b
unknown <aja.washington13@gmail.com>
2020-04-14 15:32:00 -0500

    trainingProgram

--- BangazonWorkforce/Models/ViewModels/TrainingProgramViewModel.cs
+++ BangazonWorkforce/Models/ViewModels/TrainingProgramViewModel.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace BangazonWorkforce.Models
+namespace BangazonWorkforce.Models.ViewModels
 {
-    public class ProductType
+    public class TrainingProgramViewModel
     {
+
         public int Id { get; set; }
         public string Name { get; set; }
-        public List<Product> Products { get; set; }
+
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
     }
 }
