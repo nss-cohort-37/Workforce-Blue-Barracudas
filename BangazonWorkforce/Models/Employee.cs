9b030abe28b4c6fae53daeca4ca3f1ae541d393d
Jasmine <jasminewilliams2017@gmail.com>
2020-04-14 17:44:57 -0500

    employees list view complete

--- BangazonWorkforce/Models/Employee.cs
+++ BangazonWorkforce/Models/Employee.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace BangazonAPI.Models
+namespace BangazonWorkforce.Models
 {
     public class Employee
     {
+     
         public int Id { get; set; }
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
         public int DepartmentId { get; set; }
         public string Email { get; set; }
@@ -16,7 +20,7 @@ namespace BangazonAPI.Models
         public int? ComputerId { get; set; }
         public Computer Computer { get; set; }
         
-
+        public Department Department { get; set; }
 
     }
 }
